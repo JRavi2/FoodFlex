d03e5a449d6ed7d52cdc45b519da62451ddf22b4
Ravi Jain <jravi248@gmail.com>
2021-07-31 23:08:43 +0530

    Add Home page (for sure this time :')

---
+++ src/pages/Home.tsx
@@ -0,0 +1,29 @@
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+// import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+
+const Page: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Home</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+	{/* <ExploreContainer name="Home" /> */}
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Page;
