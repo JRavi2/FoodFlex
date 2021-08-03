330af00c64e3d0cf1b360eec6551d9e787776e3b
Ravi Jain <jravi248@gmail.com>
2021-08-03 22:37:04 +0530

    Add seperate css file for Sigup page, and some random formatting

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,25 +1,37 @@
-import React, { useState, useContext, Dispatch } from 'react';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButtons, IonMenuButton, IonRow, IonCol, IonButton, IonList, IonItem, IonLabel, IonInput, IonText } from '@ionic/react';
-import './Login.css';
+import React, { useState, useContext, Dispatch } from "react";
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonPage,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+} from "@ionic/react";
+import "./Signup.css";
 // import { RouteComponentProps, Redirect } from 'react-router';
 
 const Signup: React.FC = () => {
-
-  const [username, setUsername] = useState('');
-  const [password1, setPassword1] = useState('');
-  const [password2, setPassword2] = useState('');
-  const [name, setName] = useState('');
-  const [address, setAddress] = useState('');
-  const [phone, setPhone] = useState('');
-  const [budget, setBudget] = useState('');
-  const [preferredRestaurants, setPreferredRestaurants] = useState('');
-  const [preferredCuisines, setPreferredCuisines] = useState('');
-  const [dislikedDishes, setDislikedDishes] = useState('');
+  const [username, setUsername] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [phone, setPhone] = useState("");
+  const [budget, setBudget] = useState("");
+  const [preferredRestaurants, setPreferredRestaurants] = useState("");
+  const [preferredCuisines, setPreferredCuisines] = useState("");
+  const [dislikedDishes, setDislikedDishes] = useState("");
   // const [formSubmitted, setFormSubmitted] = useState(false);
   // const [usernameError, setUsernameError] = useState(false);
   // const [passwordError, setPasswordError] = useState(false);
 
-  const login = async (e: React.FormEvent) => {
+  const login = (e: React.FormEvent) => {
     e.preventDefault();
 
     // extract form data
@@ -28,19 +40,19 @@ const Signup: React.FC = () => {
     const formdata = new FormData(target);
 
     // POST the request to Staticman's API endpoint
-    const response = await fetch(process.env.REACT_APP_API_URL + "/signup/", {
+    fetch(process.env.REACT_APP_API_URL + "/signup/", {
       method: "POST",
       // headers: {"Content-Type": "application/form-data"},
       body: formdata,
     })
-      .then(response => {
-	console.log("success");
+      .then((response) => {
+        console.log("success");
       })
-      .catch(error => {
-	console.log("error");
-	console.log(error);
+      .catch((error) => {
+        console.log("error");
+        console.log(error);
       });
-  }
+  };
 
   return (
     <IonPage id="login-page">
@@ -50,7 +62,6 @@ const Signup: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-
         <div className="login-logo">
           <img src="assets/img/appicon.svg" alt="Ionic logo" />
         </div>
@@ -58,82 +69,140 @@ const Signup: React.FC = () => {
         <form noValidate onSubmit={login}>
           <IonList>
             <IonItem>
-              <IonLabel position="stacked" color="primary">Username</IonLabel>
-              <IonInput name="username" type="text" value={username} spellCheck={false} autocapitalize="off" onIonChange={e => setUsername(e.detail.value!)}
-                required>
-              </IonInput>
+              <IonLabel position="stacked" color="primary">
+                Username
+              </IonLabel>
+              <IonInput
+                name="username"
+                type="text"
+                value={username}
+                spellCheck={false}
+                autocapitalize="off"
+                onIonChange={(e) => setUsername(e.detail.value!)}
+                required
+              ></IonInput>
             </IonItem>
 
             <IonItem>
-              <IonLabel position="stacked" color="primary">Password</IonLabel>
-              <IonInput name="password1" type="password" value={password1} onIonChange={e => setPassword1(e.detail.value!)}>
-              </IonInput>
+              <IonLabel position="stacked" color="primary">
+                Password
+              </IonLabel>
+              <IonInput
+                name="password1"
+                type="password"
+                value={password1}
+                onIonChange={(e) => setPassword1(e.detail.value!)}
+              ></IonInput>
             </IonItem>
 
             <IonItem>
-              <IonLabel position="stacked" color="primary">Confirm Password</IonLabel>
-              <IonInput name="password2" type="password" value={password2} onIonChange={e => setPassword2(e.detail.value!)}>
-              </IonInput>
+              <IonLabel position="stacked" color="primary">
+                Confirm Password
+              </IonLabel>
+              <IonInput
+                name="password2"
+                type="password"
+                value={password2}
+                onIonChange={(e) => setPassword2(e.detail.value!)}
+              ></IonInput>
             </IonItem>
 
             <IonItem>
-              <IonLabel position="stacked" color="primary">Name</IonLabel>
-              <IonInput name="name" type="text" value={name} onIonChange={e => setName(e.detail.value!)}>
-              </IonInput>
+              <IonLabel position="stacked" color="primary">
+                Name
+              </IonLabel>
+              <IonInput name="name" type="text" value={name} onIonChange={(e) => setName(e.detail.value!)}></IonInput>
             </IonItem>
 
             <IonItem>
-              <IonLabel position="stacked" color="primary">Address</IonLabel>
-              <IonInput name="address" type="text" value={address} onIonChange={e => setAddress(e.detail.value!)}>
-              </IonInput>
+              <IonLabel position="stacked" color="primary">
+                Address
+              </IonLabel>
+              <IonInput
+                name="address"
+                type="text"
+                value={address}
+                onIonChange={(e) => setAddress(e.detail.value!)}
+              ></IonInput>
             </IonItem>
 
             <IonItem>
-              <IonLabel position="stacked" color="primary">Phone</IonLabel>
-              <IonInput name="phone" type="text" value={phone} onIonChange={e => setPhone(e.detail.value!)}>
-              </IonInput>
+              <IonLabel position="stacked" color="primary">
+                Phone
+              </IonLabel>
+              <IonInput
+                name="phone"
+                type="text"
+                value={phone}
+                onIonChange={(e) => setPhone(e.detail.value!)}
+              ></IonInput>
             </IonItem>
 
             <IonItem>
-              <IonLabel position="stacked" color="primary">Budget</IonLabel>
-              <IonInput name="budget" type="number" value={budget} onIonChange={e => setBudget(e.detail.value!)}>
-              </IonInput>
+              <IonLabel position="stacked" color="primary">
+                Budget
+              </IonLabel>
+              <IonInput
+                name="budget"
+                type="number"
+                value={budget}
+                onIonChange={(e) => setBudget(e.detail.value!)}
+              ></IonInput>
             </IonItem>
 
             <IonItem>
-              <IonLabel position="stacked" color="primary">Preferred Restaurants</IonLabel>
-              <IonInput name="preferred_restaurants" type="text" value={preferredRestaurants} onIonChange={e => setPreferredRestaurants(e.detail.value!)}>
-              </IonInput>
+              <IonLabel position="stacked" color="primary">
+                Preferred Restaurants
+              </IonLabel>
+              <IonInput
+                name="preferred_restaurants"
+                type="text"
+                value={preferredRestaurants}
+                onIonChange={(e) => setPreferredRestaurants(e.detail.value!)}
+              ></IonInput>
             </IonItem>
 
             <IonItem>
-              <IonLabel position="stacked" color="primary">Preferred Cuisines</IonLabel>
-              <IonInput name="preferred_cuisines" type="text" value={preferredCuisines} onIonChange={e => setPreferredCuisines(e.detail.value!)}>
-              </IonInput>
+              <IonLabel position="stacked" color="primary">
+                Preferred Cuisines
+              </IonLabel>
+              <IonInput
+                name="preferred_cuisines"
+                type="text"
+                value={preferredCuisines}
+                onIonChange={(e) => setPreferredCuisines(e.detail.value!)}
+              ></IonInput>
             </IonItem>
 
             <IonItem>
-              <IonLabel position="stacked" color="primary">Disliked Dishes</IonLabel>
-              <IonInput name="not_preferred" type="text" value={dislikedDishes} onIonChange={e => setDislikedDishes(e.detail.value!)}>
-              </IonInput>
+              <IonLabel position="stacked" color="primary">
+                Disliked Dishes
+              </IonLabel>
+              <IonInput
+                name="not_preferred"
+                type="text"
+                value={dislikedDishes}
+                onIonChange={(e) => setDislikedDishes(e.detail.value!)}
+              ></IonInput>
             </IonItem>
 
             <IonItem>
-              <IonLabel position="stacked" color="primary">Mess Menu</IonLabel>
-	      <input type="file" name="upload_mess_menu" />
+              <IonLabel position="stacked" color="primary">
+                Mess Menu
+              </IonLabel>
+              <input type="file" name="upload_mess_menu" />
             </IonItem>
-
           </IonList>
 
           <IonRow>
             <IonCol>
-              <IonButton type="submit" expand="block">Submit</IonButton>
+              <IonButton type="submit" expand="block">
+                Submit
+              </IonButton>
             </IonCol>
           </IonRow>
         </form>
-
       </IonContent>
-
     </IonPage>
   );
 };
