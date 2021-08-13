f5d579384f6c4c072d5ce243b35850aa81cd99aa
harshita-jain28 <harshita282002@gmail.com>
2021-08-13 18:17:29 +0530

    minor fix

--- src/pages/VendorHome.tsx
+++ src/pages/VendorHome.tsx
@@ -5,16 +5,15 @@ import TransactionList from "../components/TransactionList"
 const VendorHome: React.FC = () =>{
     const [nameUser, setName] = useState<string>("Ramu");
     const [amount, setAmount] = useState<number>(100);
-    const [date, setSelectedDate] = useState<string>('2012-12-15T13:47:20.789');
+    const [date, setSelectedDate] = useState<string>('2021-12-15T13:47:20.789');
 
     return(
         <IonContent>
              <div className="login-logo">
           <img src="https://i.postimg.cc/qv3578F0/trans.png" alt="Ionic logo" /> 
         </div>
-        <TransactionList username={"Ravi"} amount={amount} date={date} ></TransactionList>
+        <TransactionList username={"Aakash"} amount={amount} date={date} ></TransactionList>
         <TransactionList username={"Kartik"} amount={amount} date={date}></TransactionList>
-        <TransactionList username={"Harshita"} amount={amount} date={date}></TransactionList>
         </IonContent>
     )
 }
